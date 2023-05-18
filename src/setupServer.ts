7d85989620fd0b77ad1f3414763eb28533bef678
mldamico <matias.lioneldamico@gmail.com>
2023-05-18 20:26:01 -0300

    Middleware configuration

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -7,7 +7,13 @@ import {
   NextFunction,
 } from "express";
 import http from "http";
-
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookieSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+import compression from "compression";
+import "express-async-errors";
 export class ChattyServer {
   private app: Application;
 
@@ -20,13 +26,39 @@ export class ChattyServer {
     this.standardMiddleware(this.app);
     this.routeMiddleware(this.app);
     this.globalHandler(this.app);
+    this.startServer(this.app);
   }
-  private securityMiddleware(app: Application): void {}
-  private standardMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["args", "args2"],
+        maxAge: 24 * 7 * 3600000,
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "50mb" }));
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
+  }
   private routeMiddleware(app: Application): void {}
   private globalHandler(app: Application): void {}
 
   private startServer(app: Application) {}
 
   private createSocketIO(httpServer: http.Server) {}
+
+  private startHttpServer(httpServer: http.Server) {}
 }
