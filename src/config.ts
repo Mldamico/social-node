1c3af35126a8c199229df71d4bbc11d1fb4a489f
mldamico <matias.lioneldamico@gmail.com>
2023-05-26 17:30:05 -0300

    Cloudinary setup

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import * as process from 'process';
 import bunyan, { LogLevel } from 'bunyan';
+import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config({});
 
@@ -12,6 +13,9 @@ class Config {
   public SECRET_KEY_TWO: string | undefined;
   public CLIENT_URL: string | undefined;
   public REDIS_HOST: string | undefined;
+  public CLOUD_NAME : string | undefined;
+  public CLOUD_API_KEY : string | undefined;
+  public CLOUD_API_SECRET : string | undefined;
 
   constructor() {
     this.DATABASE_URL = process.env.DATABASE_URL;
@@ -21,6 +25,9 @@ class Config {
     this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
     this.CLIENT_URL = process.env.CLIENT_URL || '';
     this.REDIS_HOST = process.env.REDIS_HOST || '';
+    this.CLOUD_NAME = process.env.CLOUD_NAME || '';
+    this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
+    this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
   }
 
   public validateConfig() {
@@ -33,6 +40,14 @@ class Config {
 
   public createLogger(name: string, level: LogLevel = 'debug') {
     return bunyan.createLogger({ name, level });
+  }
+
+  public cloudinaryConfig() {
+    cloudinary.config({
+      cloud_name: this.CLOUD_API_KEY,
+      api_key: this.CLOUD_API_KEY,
+      api_secret: this.CLOUD_API_SECRET
+    })
   }
 }
 
