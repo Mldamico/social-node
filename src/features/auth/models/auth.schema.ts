20eb37caf72c32c5f03994e6313d2b57d4f172fb
mldamico <matias.lioneldamico@gmail.com>
2023-05-27 16:48:51 -0300

    Sign up method controller

--- src/features/auth/models/auth.schema.ts
+++ src/features/auth/models/auth.schema.ts
@@ -4,7 +4,7 @@ import { model, Model, Schema } from 'mongoose';
 
 const SALT_ROUND = 10;
 
-const authSchema: Schema = new Schema(
+const authSchema: Schema = new Schema<IAuthDocument>(
   {
     username: { type: String },
     uId: { type: String },
@@ -13,7 +13,7 @@ const authSchema: Schema = new Schema(
     avatarColor: { type: String },
     createdAt: { type: Date, default: Date.now },
     passwordResetToken: { type: String, default: '' },
-    passwordResetExpires: { type: Number }
+    passwordResetExpires: { type: Number },
   },
   {
     toJSON: {
